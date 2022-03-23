66c42ec1c2bb9b2d56cc4914e5624ff78ce6bafa
gem-adityarajsingh <101547749+gem-adityarajsingh@users.noreply.github.com>
2022-03-23 17:13:02 +0530

    Add files via upload

---
+++ JS-file.js
@@ -0,0 +1,50 @@
+function validate() {
+  var name = document.forms["details-Form"]["name"].value;
+  var email = document.forms["details-Form"]["email"].value;
+  var mobileNumber = document.forms["details-Form"]["mobileNumber"].value;
+
+  //validating Name
+  var pattern = new RegExp(/[~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?0123456789]/); //unacceptable chars
+  if (pattern.test(name)) {
+    alert("Please only use Alphabets for name");
+    return false;
+  }
+  //Validating Mobile Number
+  if (mobileNumber.length != 10) {
+    alert("Please enter correct Mobile Number");
+    return false;
+  }
+  var isMatch = mobileNumber.match(/^[0-9\s(-)]*$/);
+  if (!isMatch) {
+    alert("The mobile number contains invalid characters.");
+    return false;
+  }
+  // Storing the data in local storage if all entries are correct
+  localStorage.setItem("name", name);
+  localStorage.setItem("email", email);
+  localStorage.setItem("mobileNumber", mobileNumber);
+}
+
+function create_table() {
+  let tableBody = document.getElementById("tableBody");
+  let tr = document.createElement("tr");
+  
+  //creating first (name) node
+  let td1 = document.createElement("td");
+  let nameNode = document.createTextNode(localStorage.getItem("name"));
+  td1.appendChild(nameNode);
+ 
+  //creating second (email) node
+  let td2 = document.createElement("td");
+  let emailNode = document.createTextNode(localStorage.getItem("email"));
+  td2.appendChild(emailNode);
+
+  //creating third (mobile number) node
+  let td3 = document.createElement("td");
+  let mobileNumberNode = document.createTextNode(localStorage.getItem("mobileNumber"));
+  td3.appendChild(mobileNumberNode);
+  tr.append(td1);
+  tr.append(td2);
+  tr.append(td3);
+  tableBody.append(tr);
+}
