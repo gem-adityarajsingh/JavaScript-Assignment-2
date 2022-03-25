18f2da586980c8349140424962735ef802fd09a4
gem-adityarajsingh <101547749+gem-adityarajsingh@users.noreply.github.com>
2022-03-25 11:21:14 +0530

    Update JS-file.js

--- JS-file.js
+++ JS-file.js
@@ -2,32 +2,68 @@ function validate() {
   var name = document.forms["details-Form"]["name"].value;
   var email = document.forms["details-Form"]["email"].value;
   var mobileNumber = document.forms["details-Form"]["mobileNumber"].value;
+  var valName = true;
+  var valEmail = true;
+  var valNumber = true;
   //validating Name
   var pattern = new RegExp(/[~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?0123456789]/); //unacceptable chars
   if (pattern.test(name)) {
-    alert("Please only use Alphabets for name");
+    //alert("Please only use Alphabets for name");
+    //return false;
+    valName = false;
   }
   //Validating Mobile Number
   if (mobileNumber.length > 0 && mobileNumber.length != 10) {
-    alert("Please enter correct Mobile Number");
+    //alert("Please enter correct Mobile Number");
+    //return false;
+    valNumber = false;
   }
   var isMatch = mobileNumber.match(/^[0-9\s(-)]*$/);
   if (!isMatch) {
-    alert("The mobile number contains invalid characters.");
+    //alert("The mobile number contains invalid characters.");
+    //return false;
+    valNumber = false;
   }
   // Validating Email
   var atPosition = email.indexOf("@");                                            
   var dotPosition = email.lastIndexOf(".");
   if (atPosition < 1 || dotPosition < atPosition + 2 || dotPosition + 2 >= email.length) {
-    alert("Invalid email address.");
-    return false;
+    //alert("Invalid email address.");
+    //return false;
+    valEmail = false;
   } 
+  if(valName === false || valEmail === false || valNumber === false){
+    if(valName === false && valEmail === false && valNumber === false){
+      alert("All details are invalid");
+    }
+    else if(valName && valEmail){
+      alert("Invalid Mobile Number");
+    }
+    else if(valName && valNumber){
+      alert("Invalid Email");
+    }
+    else if(valEmail && valNumber){
+      alert("Invalid Name");
+    }
+    else if(valName){
+      alert("Invalid Email and Mobile Number");
+    }
+    else if(valEmail){
+      alert("Invalid Name and Mobile NUmber");
+    }
+    else if(valNumber){
+      alert("Invalid Name and Email");
+    }
+    return false;
+  }
+  else{
   // Storing the data in local storage if all entries are correct
   localStorage.setItem("name", name);
   localStorage.setItem("email", email);
   localStorage.setItem("mobileNumber", mobileNumber);
   create_table();
   //return true;
+}
 }
 
 
